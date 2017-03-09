28556776d869e80fe7d561cdd9b865b3bf43d776
Meelis Pinka <surmus@gmail.com>
2017-03-09 08:48:57 +0200

    remove hardcoded type from interface

--- src/main/java/com/matrix_crypto/service/MatrixMultiplicationService.java
+++ src/main/java/com/matrix_crypto/service/MatrixMultiplicationService.java
@@ -3,5 +3,5 @@ package com.matrix_crypto.service;
 import com.matrix_crypto.model.Matrix;
 
 public interface MatrixMultiplicationService<T> {
-    Matrix<Double> multiply(Matrix<T> a, Matrix<T> b);
+    Matrix<T> multiply(Matrix<T> a, Matrix<T> b);
 }
